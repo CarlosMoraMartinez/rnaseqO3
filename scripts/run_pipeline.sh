b35916e9177d981d46db4f38b2a46d454fe8bf04
Carlos Mora Martínez <carmoma9@gmail.com>
2024-07-05 13:10:27 +0200

    Added some memory

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -1,3 +1,5 @@
 sudo nextflow run main.nf -c config/run_samples_local_cmm_docker.config -resume
 
 sudo nextflow run main.nf -c config/run_samples_local_cmm_docker.config -profile conda -resume -with-timeline timeline.html -with-report report.html -with-dag pipeline_dag.html
+
+sudo /home/servidor/PROGRAMAS/nextflow run main.nf -c config/run_mm1_local_1.config -profile conda -resume -with-timeline timeline.html -with-report report.html -with-dag pipeline_dag.html
