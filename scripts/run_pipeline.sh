1013bbd9e79c2b59e788c853a46c55210c621cb5
Carlos Mora Martínez <carmoma9@gmail.com>
2025-09-09 23:55:56 +0200

    added workdir

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -7,3 +7,8 @@ sudo /home/servidor/PROGRAMAS/nextflow run main.nf -c config/run_mm1_local_1.con
 
 sudo rm *.html
 sudo /home/servidor/PROGRAMAS/nextflow run main.nf -c config/run_mm1_local_sim.config -resume -with-timeline timeline.html -with-report report.html -with-dag pipeline_dag.htmlne_dag.html
+
+# git switch -c feature/evaMouseSept2025_2 origin/feature/evaMouseSept2025_2
+# /home/servidor/bin/nextflow on server 3
+
+sudo /home/servidor/bin/nextflow  run main.nf -c config/run_evaMouseSept25_docker.config -resume -with-timeline timeline.html -with-report report.html -with-dag pipeline_dag.htmlne_dag.html
